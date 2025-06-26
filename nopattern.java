bf96dd3c184ee261c28cef51bdc892c9027c294c
yashchavan5 <yashchavanpatil1@gmail.com>
2025-06-26 11:47:22 +0530

    Update nopattern.java

--- nopattern.java
+++ nopattern.java
@@ -184,3 +184,109 @@ class Product{
 
 
 
+// Java Program to print
+// Pyramid pattern
+/*import java.util.*;
+
+public class Geeks {
+    
+    
+    public static void printPattern(int n)
+    {
+        int i, j;
+
+        
+        for (i = 1; i <= n; i++) {
+
+           
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+           
+            System.out.println();
+        }
+    }
+
+   
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}*/
+
+
+// Java Program to print pattern
+// Reverse Right Half Pyramid
+/*import java.util.*;
+
+public class Geeks {
+    
+    
+    public static void printPattern(int n)
+    {
+        int i, j;
+
+       
+        for (i = n; i >= 1; i--) {
+
+            // inner loop to handle columns
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+           
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}*/
+
+
+// Java Program to print pattern
+// Square hollow pattern
+/*import java.util.*;
+
+public class Geeks {
+    
+    public static void printPattern(int n)
+    {
+        int i, j;
+        
+       
+        for (i = 0; i < n; i++) {
+            
+            
+            for (j = 0; j < n; j++) {
+                
+               
+                if (i == 0 || j == 0 || i == n - 1
+                    || j == n - 1) {
+                    System.out.print("*");
+                }
+                
+                
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+   
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}*/
+
+
